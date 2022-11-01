ccace8d5177c8c713a5b90d0cde839f3a4587cfc
Mr.Jirakit Athchasiri <ken_jirakit@hotmail.com>
2022-11-01 15:24:17 +0700

    first commit

---
+++ src/pages/Search.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import SearchCard from "../components/SearchCard";
+import Sidebar from "../components/Sidebar";
+import Spinner from "../components/Spinner";
+import { clearVideos } from "../store";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { getSearchPageVideos } from "../store/reducers/getSearchPageVideos";
+import { HomePageVideos } from "../Types";
+
+export default function Search() {
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(clearVideos());
+    if (searchTerm === "") navigate("/");
+    else {
+      dispatch(getSearchPageVideos(false));
+    }
+  }, [dispatch, navigate, searchTerm]);
+  return (
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <Sidebar />
+        {videos.length ? (
+          <div className="py-8 pl-8 flex flex-col gap-5 w-full">
+            <InfiniteScroll
+              dataLength={videos.length}
+              next={() => dispatch(getSearchPageVideos(true))}
+              hasMore={videos.length < 500}
+              loader={<Spinner />}
+              height={600}
+            >
+              {videos.map((item: HomePageVideos) => {
+                return (
+                  <div className="my-5">
+                    <SearchCard data={item} key={item.videoId} />
+                  </div>
+                )
+              })}
+            </InfiniteScroll>
+          </div>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
+  );
+}
